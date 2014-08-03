a415b74cb47fbca7b879ef0e6249dd02337040d3
Dominic Muller <nicklink483@gmail.com>
2014-08-03 10:51:49 -0700

    version 0.0.4

--- lib/sound/out.rb
+++ lib/sound/out.rb
@@ -4,7 +4,7 @@ module Sound
   module Out
   
     def self.play_freq(frequency = 440, duration = 1000, volume = 1, immediate_playback = true)
-      if Platform.eql? "i386-mingw32"
+      if OS.windows?
         Win32::Sound.play_freq(frequency, duration, volume, immediate_playback)
       else
         warn("play_freq not implemented for this platform: #{RUBY_PLATFORM}")
