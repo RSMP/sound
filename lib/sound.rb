6665b68bef1c39d7c82c42198d3dafb1a8e0be5d
Dominic Muller <nicklink483@gmail.com>
2014-08-10 14:10:05 -0700

    linux sound library load fix

--- lib/sound.rb
+++ lib/sound.rb
@@ -5,6 +5,8 @@ require 'os/os'
 
 if OS.windows?
   require 'sound/win32/sound'
+elsif OS.linux?
+  require 'sound/linux/sound'
 else
   warn("warning: Sound output not yet implemented for this platform: #{OS.os}")
 end
