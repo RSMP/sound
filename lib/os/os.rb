7b8f48db533e8319af21f369afaac26643e11994
Dominic Muller <nicklink483@gmail.com>
2014-08-14 12:32:16 -0700

    added specs for OS

--- lib/os/os.rb
+++ lib/os/os.rb
@@ -1,5 +1,9 @@
 module OS
 
+  class << self
+    attr_accessor :os
+  end
+
   def OS.windows?
     os == :windows
   end
@@ -17,21 +21,22 @@ module OS
   end
   
   def OS.os
-    @os ||= (
-      host_os = RbConfig::CONFIG['host_os']
-      case host_os
-      when /mswin|msys|mingw|cygwin|bccwin|wince|emc|emx/
-        :windows
-      when /darwin|mac os/
-        :mac
-      when /linux/
-        :linux
-      when /solaris|bsd/
-        :unix
-      else
-        raise Error::WebDriverError, "unknown os: #{host_os.inspect}"
-      end
-    )
+    @os ||= OS.choose_os
+  end
+  
+  def OS.choose_os
+    case RbConfig::CONFIG['host_os']
+    when /mswin|msys|mingw|cygwin|bccwin|wince|emc|emx/
+      :windows
+    when /darwin|mac os/
+      :mac
+    when /linux/
+      :linux
+    when /solaris|bsd/
+      :unix
+    else
+      raise Error::WebDriverError, "unknown os: #{RbConfig::CONFIG['host_os'].inspect}"
+    end
   end
 
 end
